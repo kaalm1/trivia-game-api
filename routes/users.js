8da79579a142e43cfd61d3537f5f13933507c8e4
Joe <kaalm1@gmail.com>
2017-12-14 08:13:36 +0200

    added user

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/new', function(req, res, next) {
+  let data = req.body
+  users.insert(data)
+
+  // res.send('respond with a resource');
+});
+
 module.exports = router;
